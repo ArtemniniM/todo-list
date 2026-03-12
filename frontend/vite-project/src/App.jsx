b5f8421966571fb2e27113c450aa9a41e530c443
Artem Minin <artemminin.03@mail.ru>
2026-03-12 21:53:22 +0300

    add modal

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -8,14 +8,24 @@ function App() {
   const [array, setArray] = useState([]);
   const [input1, setInput1] = useState("");
   const [input2, setInput2] = useState("");
+  const [modal, setModal] = useState(false);
+
+  const modalShow = () => {
+    setModal(true);
+  };
+
+  const cancelBtn = () => {
+    setModal(false);
+  };
+
   async function getAllData() {
     const response = await axios.get("http://localhost:3000/tasks/");
-    setArray(response.data);
+    setArray([...array, ...response.data]);
   }
   async function createData() {
     try {
-      if (!input1.length) throw new Error("Title empty");
-      if (!input2.length) throw new Error("Description empty");
+      if (!input1.length) throw new Error("title empty");
+      if (!input2.length) throw new Error("description empty");
 
       const response = await axios.post("http://localhost:3000/tasks/", {
         title: input1,
@@ -24,7 +34,7 @@ function App() {
         createdat: "2024-01-25T11:30:00.000Z",
       });
       console.log(response);
-      setArray(response.data);
+      setArray([...array, ...response.data]);
     } catch (error) {
       console.log(error.message);
     }
@@ -63,11 +73,23 @@ function App() {
                 <h4>{el.title}</h4>
                 <p>{el.description}</p>
                 <div className={style.iconsContainer}>
-                  <img src={image_1}></img>
+                  <img onClick={modalShow} src={image_1}></img>
                   <img src={image_2}></img>
                 </div>
               </div>
             ))}
+          </div>
+        </div>
+        <div className={style.overlay} style={{ display: modal ? "block" : "none" }} />
+        <div className={style.modalContainer} style={{ display: modal ? "block" : "none" }}>
+          <h1>Update Note</h1>
+          <input type="text" placeholder="Input your note..." />
+          <input type="text" placeholder="Input your description note..." />
+          <div className={style.buttonWrapper}>
+            <button className={style.cancelBtn} onClick={cancelBtn}>
+              Cancel
+            </button>
+            <button className={style.applyBtn}>Apply</button>
           </div>
         </div>
       </div>
